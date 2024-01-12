3082e13dbc9e40ef7f0b3420117aea1d60434a7d
alvarom4ta <156408177+alvarom4ta@users.noreply.github.com>
2024-01-12 15:27:53 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("D9nvfC4oHpgaZJWj4ejgyhTftCcPna9nKMgyJZAJdaAS");
 
 #[program]
 pub mod solquad {
